7cd5c0572de82c5b3d9ebe5c380dd44d35e79dc6
bee <beelan@yeah.net>
2026-05-23 00:25:03 +0800

    Add gatewayhub cask v0.0.6-beta.2

---
+++ Casks/gatewayhub.rb
@@ -0,0 +1,32 @@
+cask "gatewayhub" do
+  version "0.0.6-beta.2"
+  sha256 "d4b79c2b55f0e57da79971b97363443c513600921121ecd328fabb3aa1cc4f3a"
+
+  url "https://github.com/bee1an/GatewayHub/releases/download/v#{version}/gatewayhub-#{version}.dmg"
+  name "GatewayHub"
+  desc "Local multi-provider AI gateway hub with Kiro account pool"
+  homepage "https://github.com/bee1an/GatewayHub"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates false
+  depends_on macos: ">= :big_sur"
+
+  app "GatewayHub.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-cr", "#{appdir}/GatewayHub.app"],
+                   sudo: false
+  end
+
+  zap trash: [
+    "~/Library/Application Support/GatewayHub",
+    "~/Library/Logs/GatewayHub",
+    "~/Library/Preferences/dev.gatewayhub.app.plist",
+    "~/Library/Saved Application State/dev.gatewayhub.app.savedState",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,22 @@
+# homebrew-gatewayhub
+
+Homebrew tap for [GatewayHub](https://github.com/bee1an/GatewayHub).
+
+## Install
+
+```sh
+brew tap bee1an/gatewayhub
+brew install --cask gatewayhub
+```
+
+## Upgrade
+
+```sh
+brew upgrade --cask gatewayhub
+```
+
+## Uninstall
+
+```sh
+brew uninstall --cask gatewayhub
+```
